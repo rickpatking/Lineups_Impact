853acfaf35db2697f356cbdce0985bcca67898e0
rickpatking <patrickking@ufl.edu>
2025-10-18 14:17:16 -0400

    Did more work on the structure of everything, creating the functions for getting and automating data. Going with a method that uses classes to keep everythign organized. Need to focus more on the sql side of things next

--- src/etl/lineup_tracker.py
+++ src/etl/lineup_tracker.py
@@ -2,100 +2,102 @@ import pandas as pd
 import numpy as np
 from nba_api.stats.endpoints import CommonTeamRoster
 
-def clean_pbp(playbyplay, team_id):
-    subs = playbyplay[playbyplay['actionType'] == 'Substitution'].copy()
+class LineupTracker:
 
-    subs['prev_name'] = subs['description'].str.split(r': | FOR').str[2]
-    subs['prev_name'] = subs['prev_name'].str.lstrip()
-    subs['prev_id'] = subs['personId']
-    subs['next_name'] = subs['description'].str.split(r': | FOR').str[1]
-    subs['next_name'] = subs['next_name'].str.lstrip()
-    subs['next_id'] = None
-    
-    all_subs = subs[subs['teamTricode'] == 'IND'].copy()
-    sorted_prev = np.sort(all_subs['prev_name'].unique())
-    sorted_next = np.sort(all_subs['next_name'].unique())
+    def clean_pbp(self, playbyplay, team_id):
+        subs = playbyplay[playbyplay['actionType'] == 'Substitution'].copy()
 
-    intersection = np.intersect1d(sorted_prev, sorted_next)
-    difference = np.setdiff1d(sorted_prev, sorted_next)
-    difference2 = np.setdiff1d(sorted_next, sorted_prev)
-    all_players = np.concatenate((intersection, difference, difference2), axis=0)
+        subs['prev_name'] = subs['description'].str.split(r': | FOR').str[2]
+        subs['prev_name'] = subs['prev_name'].str.lstrip()
+        subs['prev_id'] = subs['personId']
+        subs['next_name'] = subs['description'].str.split(r': | FOR').str[1]
+        subs['next_name'] = subs['next_name'].str.lstrip()
+        subs['next_id'] = None
+        
+        all_subs = subs[subs['teamTricode'] == 'IND'].copy()
+        sorted_prev = np.sort(all_subs['prev_name'].unique())
+        sorted_next = np.sort(all_subs['next_name'].unique())
 
-    player_id_dict = dict()
-    for player in all_players:
-        try:
-            playerdf = playbyplay[playbyplay['playerName'] == player]
-            id = ((playerdf.iloc[0])['personId'])
-            player_id_dict[player] = id
-        except Exception as e: 
-            team_roster = CommonTeamRoster(team_id=1610612754, season='2024-25')
-            roster_df = team_roster.get_data_frames()[0]
-            roster_df2 = (roster_df[roster_df['PLAYER'].str.contains(player)]).copy()
-            player_id_dict[player] = roster_df2['PLAYER_ID'].iloc[0]
+        intersection = np.intersect1d(sorted_prev, sorted_next)
+        difference = np.setdiff1d(sorted_prev, sorted_next)
+        difference2 = np.setdiff1d(sorted_next, sorted_prev)
+        all_players = np.concatenate((intersection, difference, difference2), axis=0)
 
-    for player, id in player_id_dict.items():
-        subs.loc[subs['next_name'] == player, 'next_id'] = id
-
-    return subs
-
-def get_quarter_starters(playbyplay, subs, quarter, team_id):
-    """
-    Returns a list of the 5 players on the court at the start of a quarter
-    playbyplay: playbyplay df that is cleaned
-    quarter: int quarter to search
-    team_id: int team_id
-    """
-    df = playbyplay[playbyplay['period'] == quarter].copy()
-    df2 = df[df['teamId'] == team_id].copy()
-    first_sub = df2[df2['actionType'] == 'Substitution'].index[0]
-    df3 = df2.loc[:df2.index[df2.index.get_loc(first_sub)-1]]
-    unique_ids = df3['personId'].unique()
-    if len(unique_ids) < 5:
-        subs = subs[subs['period'] == quarter].copy()
-        first_sub_series = subs[subs['teamId'] == team_id].copy()
-        while True:
-            sub_series = first_sub_series.iloc[0]
-            if sub_series['prev_id'] not in unique_ids.tolist():
-                unique_ids = np.append(unique_ids, sub_series['prev_id'])
-                return (unique_ids.tolist())
-            first_sub_series = first_sub_series.iloc[1:]
-
-        # more_events = df.head(50)
-        # more_uniques = more_events['personId'].unique()
-        # missed_players = np.setdiff1d(more_uniques, unique_ids)
-        # missed_players = missed_players.tolist()
-        # for i in range(len(missed_players)):
-        #     if missed_players[i] != first_sub_series['next_id']:
-        #         unique_ids = np.append(unique_ids, first_sub_series['prev_id'])
-        #         return (unique_ids.tolist())
-    return (unique_ids.tolist())
-
-def get_lineups(playbyplay, subs, team_id):
-    all_lineups = []
-    lineups_action_id = []
-
-    team_subs = subs.loc[subs['teamId'] == team_id]
-    for i in range(1, 5):
-
-        quarter = team_subs[team_subs['period'] == i].copy()
-        starters = get_quarter_starters(playbyplay, subs, i, team_id)
-        all_lineups.append(starters)
-        lineups_action_id.append(team_subs['actionId'].iloc[0])
-
-        for index, sub in quarter.iterrows():
+        player_id_dict = dict()
+        for player in all_players:
             try:
-                current_five = all_lineups[-1][:]
-                current_five.remove(sub['prev_id'])
-                current_five.append(sub['next_id'])
-                all_lineups.append(current_five)
-                lineups_action_id.append(index+1)
-            except Exception as e:
-                print(e)
-                print(index)
-                print(current_five)
-                print(all_lineups[-1][:])
-                print()
-    return all_lineups, lineups_action_id
+                playerdf = playbyplay[playbyplay['playerName'] == player]
+                id = ((playerdf.iloc[0])['personId'])
+                player_id_dict[player] = id
+            except Exception as e: 
+                team_roster = CommonTeamRoster(team_id=1610612754, season='2024-25')
+                roster_df = team_roster.get_data_frames()[0]
+                roster_df2 = (roster_df[roster_df['PLAYER'].str.contains(player)]).copy()
+                player_id_dict[player] = roster_df2['PLAYER_ID'].iloc[0]
+
+        for player, id in player_id_dict.items():
+            subs.loc[subs['next_name'] == player, 'next_id'] = id
+
+        return subs
+
+    def get_quarter_starters(self, playbyplay, subs, quarter, team_id):
+        """
+        Returns a list of the 5 players on the court at the start of a quarter
+        playbyplay: playbyplay df that is cleaned
+        quarter: int quarter to search
+        team_id: int team_id
+        """
+        df = playbyplay[playbyplay['period'] == quarter].copy()
+        df2 = df[df['teamId'] == team_id].copy()
+        first_sub = df2[df2['actionType'] == 'Substitution'].index[0]
+        df3 = df2.loc[:df2.index[df2.index.get_loc(first_sub)-1]]
+        unique_ids = df3['personId'].unique()
+        if len(unique_ids) < 5:
+            subs = subs[subs['period'] == quarter].copy()
+            first_sub_series = subs[subs['teamId'] == team_id].copy()
+            while True:
+                sub_series = first_sub_series.iloc[0]
+                if sub_series['prev_id'] not in unique_ids.tolist():
+                    unique_ids = np.append(unique_ids, sub_series['prev_id'])
+                    return (unique_ids.tolist())
+                first_sub_series = first_sub_series.iloc[1:]
+
+            # more_events = df.head(50)
+            # more_uniques = more_events['personId'].unique()
+            # missed_players = np.setdiff1d(more_uniques, unique_ids)
+            # missed_players = missed_players.tolist()
+            # for i in range(len(missed_players)):
+            #     if missed_players[i] != first_sub_series['next_id']:
+            #         unique_ids = np.append(unique_ids, first_sub_series['prev_id'])
+            #         return (unique_ids.tolist())
+        return (unique_ids.tolist())
+
+    def get_lineups(self, playbyplay, subs, team_id):
+        all_lineups = []
+        lineups_action_id = []
+
+        team_subs = subs.loc[subs['teamId'] == team_id]
+        for i in range(1, 5):
+
+            quarter = team_subs[team_subs['period'] == i].copy()
+            starters = self.get_quarter_starters(playbyplay, subs, i, team_id)
+            all_lineups.append(starters)
+            lineups_action_id.append(team_subs['actionId'].iloc[0])
+
+            for index, sub in quarter.iterrows():
+                try:
+                    current_five = all_lineups[-1][:]
+                    current_five.remove(sub['prev_id'])
+                    current_five.append(sub['next_id'])
+                    all_lineups.append(current_five)
+                    lineups_action_id.append(index+1)
+                except Exception as e:
+                    print(e)
+                    print(index)
+                    print(current_five)
+                    print(all_lineups[-1][:])
+                    print()
+        return all_lineups, lineups_action_id
 
 # testing
 # df = pd.read_csv('data/raw/thunder_pacers_game7.csv')
--- src/etl/nba_data_extractor.py
+++ src/etl/nba_data_extractor.py
@@ -1,23 +1,47 @@
 import pandas as pd
 from nba_api.stats.endpoints import playbyplayv3, leaguegamefinder, commonteamroster
-from nba_api.stats.static import teams
+from nba_api.stats.static import teams, players
 import time
 
 class NBADataExtractor:
     def __init__(self, rate_limiter):
-        """Initialize with rate limiter"""
+        self.rate_limiter = rate_limiter
         
     def get_season_games(self, season='2023-24'):
-        """Get all games for a season"""
+        self.rate_limiter.wait_if_needed()
+
+        try:
+            gamefinder = leaguegamefinder.LeagueGameFinder(season_nullable=season)
+            games_df = gamefinder.get_data_frames()[0]
+            self.rate_limiter.record_request()
+            print(f'Successfully got games for {season}')
+            return games_df
+        except Exception as e:
+            print(e)
+            return None
         
     def get_game_playbyplay(self, game_id):
-        """Get play-by-play for one game"""
+        self.rate_limiter.wait_if_needed()
+
+        try:
+            pbpfinder = playbyplayv3.PlayByPlayV3(game_id)
+            pbp_df = pbpfinder.get_data_frames()[0]
+            self.rate_limiter.record_request()
+            print(f'Successfully got pbp data for {game_id}')
+            return pbp_df
+
+        except Exception as e:
+            print(e)
+            return None
         
     def get_teams(self):
-        """Get all NBA teams"""
+        teams_list = teams.get_teams()
+        teams_df = pd.DataFrame(teams_list)
+        print(f'Got all nba teams')
+        return teams_df
         
     def get_players(self, season='2023-24'):
-        """Get all players for a season"""
-        
-    def get_box_score(self, game_id):
-        """Get box score (for starters and validation)"""
+        players_list = players.get_active_players()
+        players_df = pd.DataFrame(players_list)
+        print(f'Got all active players')
+        return players_df
